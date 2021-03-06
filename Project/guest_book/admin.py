8f123add1e7244d43385381d40c5a34bdaee4242
A-miin <zulaykaisaeva@gmail.com>
2021-03-06 12:19:40 +0600

    fix model, register model in admin, migrations

--- Project/guest_book/admin.py
+++ Project/guest_book/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Record
 
 # Register your models here.
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at', 'updated_at', 'status']
+    list_filter = ['name', 'email']
+    search_fields = ['name', 'text']
+    fields = ['id', 'name', 'email', 'text', 'created_at', 'updated_at', 'status']
+    readonly_fields = ['created_at', 'updated_at', 'id']
+
+admin.site.register(Record, RecordAdmin)
