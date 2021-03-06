66c8de97c8ebfb283a8dfce2398dd63c878ebd04
A-miin <zulaykaisaeva@gmail.com>
2021-03-06 14:10:54 +0600

    Add update

--- Project/guest_book/views.py
+++ Project/guest_book/views.py
@@ -29,6 +29,23 @@ def record_add(request):
             return redirect('record-list')
         return render(request, 'record_add.html', context={'form':form})
 
-
-
-
+def record_update(request, pk):
+    record = get_object_or_404(Record, id=pk)
+    if request.method=='GET':
+        form=RecordForm(
+            initial={
+             'name':record.name,
+             'email':record.email,
+             'text':record.text
+            }
+        )
+        return render(request, 'record_update.html',context={'record':record,'form':form})
+    elif request.method == "POST":
+        form = RecordForm(data=request.POST)
+        if form.is_valid():
+            record.name = form.cleaned_data.get('name')
+            record.email = form.cleaned_data.get('email')
+            record.text = form.cleaned_data.get('text')
+            record.save()
+            return redirect('record-list')
+        return render(request, 'record_update.html', context={'record':record, 'form':form})
