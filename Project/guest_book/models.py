89c6d84748ef2eff2e5294050f41bc78db8932d0
A-miin <zulaykaisaeva@gmail.com>
2021-03-06 12:08:31 +0600

    Add model of records

--- Project/guest_book/models.py
+++ Project/guest_book/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+status_choices = [('active', 'Активно'), ('blocked', 'Заблокировано')]
 # Create your models here.
+class Record(models.Model):
+    name = models.CharField(null=False, blank=False, verbose_name="Имя автора")
+    email = models.EmailField(null=False, blank=False, verbose_name="Почта автора записи")
+    text = models.TextField(null=False, blank=False, verbose_name="Текст записи")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(null=False, blank=False,choices=status_choices, default='active' )
+
+    class Meta:
+        db_table='records'
+        verbose_name="Запись"
+        verbose_name_plural = "Записи"
+
+    def __str__(self):
+        return f'{self.id}. {self.name}({self.email})'
